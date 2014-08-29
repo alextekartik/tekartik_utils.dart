51f42b440f978c46cab2a0ca2409cc9718729fa2
alex <alex@tekartik.com>
2014-08-29 18:05:41 +0200

    extracting jsobject converter

--- lib/js_utils.dart
+++ lib/js_utils.dart
@@ -5,6 +5,8 @@ import 'dart:async';
 import 'dart:html';
 import 'dart:typed_data';
 
+part 'src/js_utils/jsobject_converter.dart';
+
 Future debugLoadJavascriptScript(String src) {
   Completer completer = new Completer();
   var script = new ScriptElement();
@@ -57,53 +59,6 @@ Future loadJavascriptScript(String src) {
   return completer.future;
 }
 
-List<String> jsObjectKeys(JsObject jsObject) {
-  if (jsObject is JsArray) {
-    throw new ArgumentError('object is an array');
-  }
-  JsArray jsKeys = context['Object'].callMethod('keys', [jsObject]);
-  List<String> keys = new List();
-  for (int i = 0; i < jsKeys.length; i++) {
-    keys.add(jsKeys[i]);
-  }
-  return keys;
-}
-
-/**
- * For JsObject of JsArray
- */
-dynamic jsObjectAsCollection(JsObject jsObject) {
-  if (jsObject is JsArray) {
-    return jsArrayAsList(jsObject);
-  }
-  return jsObjectAsMap(jsObject);
-}
-
-List jsArrayAsList(JsArray jsArray) {
-  List list = new List();
-  for (int i = 0; i < jsArray.length; i++) {
-    var value = jsArray[i];
-    if (value is JsObject) {
-      value = jsObjectAsCollection(value);
-    }
-    list.add(value);
-  }
-  return list;
-}
-
-Map jsObjectAsMap(JsObject jsObject) {
-  List<String> keys = jsObjectKeys(jsObject);
-  Map map = {};
-  for (String key in keys) {
-    var value = jsObject[key];
-    if (value is JsObject) {
-      // recursive
-      value = jsObjectAsCollection(value);
-    }
-    map[key] = value;
-  }
-  return map;
-}
 
 String jsRuntimeType(JsObject jsObject) {
   return jsObject['constructor']['name'].toString();
