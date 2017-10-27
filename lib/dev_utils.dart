b1cc4891356537016669de5a09defcfc99ca8d66
Alexandre Roux Tekartik <alex@tekartik.com>
2017-10-27 11:32:47 +0200

    wip

--- lib/dev_utils.dart
+++ lib/dev_utils.dart
@@ -1,3 +1,3 @@
 library tekartik_utils_dev;
 
-export 'package:tekartik_core/dev_utils.dart';
+export 'package:tekartik_common_utils/dev_utils.dart';
--- lib/log_utils.dart
+++ lib/log_utils.dart
@@ -1,4 +1,4 @@
 @deprecated // 2014-11
 library tekartik_utils_log;
 
-export 'package:tekartik_core/log_utils.dart';
+export 'package:tekartik_common_utils/log_utils.dart';
